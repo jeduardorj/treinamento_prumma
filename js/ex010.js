8b3d10551976a1932d9bd90ebb4618b08a7ecec4
Eduardo Oliveira <zeduardo.aguiar@gmail.com>
2024-03-20 19:24:15 -0300

    refatoração de codigo

--- js/ex010.js
+++ js/ex010.js
@@ -1,8 +1,35 @@
+// function mudarDeCor() {
+//     let entradaDoUsuario = prompt("Digite uma cor em hexadecimal (#ff0000) ou em nome em inglês (red):");
+
+    
+//     if (entradaDoUsuario === null) {
+//         return;
+//     }
+
+   
+//     entradaDoUsuario = entradaDoUsuario.trim();
+
+    
+//     let validaHexadecimal = /^#[0-9A-F]{6}$/i.test(entradaDoUsuario);
+//     let corEmIngles = /^(aqua|black|blue|fuchsia|gray|green|lime|maroon|navy|olive|orange|purple|red|silver|teal|white|yellow)$/i.test(entradaDoUsuario);
+
+//     if (validaHexadecimal || corEmIngles) {
+//         document.body.style.backgroundColor = entradaDoUsuario;
+//     } else {
+//         alert("Por favor, insira uma cor válida em hexadecimal (#ff0000) ou um nome de cor em inglês (red).");
+//     }
+// }
+
+
+// window.onload = mudarDeCor;
+
+//Codigo Refatorado
+
 function mudarDeCor() {
     let entradaDoUsuario = prompt("Digite uma cor em hexadecimal (#ff0000) ou em nome em inglês (red):");
 
     
-    if (entradaDoUsuario === null) {
+    if (entradaDoUsuario === null || entradaDoUsuario.trim() === "") {
         return;
     }
 
@@ -10,15 +37,23 @@ function mudarDeCor() {
     entradaDoUsuario = entradaDoUsuario.trim();
 
     
-    let validaHexadecimal = /^#[0-9A-F]{6}$/i.test(entradaDoUsuario);
-    let corEmIngles = /^(aqua|black|blue|fuchsia|gray|green|lime|maroon|navy|olive|orange|purple|red|silver|teal|white|yellow)$/i.test(entradaDoUsuario);
+    let corHexadecimal = entradaDoUsuario.startsWith("#") && (entradaDoUsuario.length === 7 || entradaDoUsuario.length === 4); 
+    let hexChars = "0123456789ABCDEFabcdef";
+    for (let i = 1; i < entradaDoUsuario.length; i++) {
+        if (!hexChars.includes(entradaDoUsuario[i])) {
+            corHexadecimal = false;
+            break;
+        }
+    }
 
-    if (validaHexadecimal || corEmIngles) {
+    
+    let corEmIngles = ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "orange", "purple", "red", "silver", "teal", "white", "yellow"].includes(entradaDoUsuario.toLowerCase());
+
+    if (corHexadecimal || corEmIngles) {
         document.body.style.backgroundColor = entradaDoUsuario;
     } else {
         alert("Por favor, insira uma cor válida em hexadecimal (#ff0000) ou um nome de cor em inglês (red).");
     }
 }
 
-
-window.onload = mudarDeCor;
+mudarDeCor()
