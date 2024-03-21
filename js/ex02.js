88433d665c30375846a1c4d6fd6ef5b6a5f331b1
Eduardo Oliveira <zeduardo.aguiar@gmail.com>
2024-03-21 02:59:31 -0300

    atualização de codigo

--- js/ex02.js
+++ js/ex02.js
@@ -1,8 +1,38 @@
-var anoClt = prompt("Em que ano você nasceu?")
+// var anoClt = prompt("Em que ano você nasceu?")
 
-var ano = new Date()
-var anoAtual = ano.getFullYear()
+// var ano = new Date()
+// var anoAtual = ano.getFullYear()
 
-var idade = anoAtual - anoClt
+// var idade = anoAtual - anoClt
 
-alert(idade)
+// alert(idade)
+
+// Data de nascimento 
+// ( curiosidade: Os exemplos formam a data de lançamento da primeita versão do javascript )
+alert("Responda as perguntas a seguir somente com os numéros respectivos.");
+
+let yearBorn = parseInt(prompt("Em qual ano você nasceu? (Exemplo: 1995)"));
+let monthBorn = parseInt(prompt("Em qual mês você nasceu? (Exemplo: 12)"));
+let dayBorn = parseInt(prompt("Em qual dia você nasceu? (Exemplo: 04)"));
+
+
+// Calculo da idade
+function calcBirth(year, month, day) {
+    // Data atual
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentDay = currentDate.getDate();
+    let birth = currentYear - year;
+    // Verificando se a idade calculada está correta e a corrigindo se não estiver 
+    if (currentMonth < month || (currentMonth == month && currentDay < day)) {
+        birth--
+    };
+    return birth
+};
+
+let age = calcBirth(yearBorn, monthBorn, dayBorn);
+
+
+// Dizendo a idade
+alert("Você tem " + age + " anos.");
--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 var nome = prompt("Digite seu nome")
 var idade = prompt(`${nome} digite sua idade`)
 
-if (idade !== Number()) {
+if (isNaN(idade)) {
     alert('Isso não é numero')
     
     idade = prompt(`${nome} digite sua idade`)
 
-    if (idade >= 18) {
-        alert(`${nome} parabens pode tirar Habilitção`)
-    } else {
-        alert(`Sinto muito ${nome} não pode tirar habilitação`)
-    }
+   
 }
+    
+if (idade >= 18) {
+        alert(`${nome} parabens pode tirar Habilitção`)
+} else {
+        alert(`Sinto muito ${nome} não pode tirar habilitação`)
+}
+
 
